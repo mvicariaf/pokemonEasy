28ba20a8683cb0d06bfbcc651a0f4ce4e4c8ce9d
mvicariaf <amivicaria@hotmail.es>
2017-07-20 16:46:38 +0200

    iteracion 1

--- js/pokemon.js
+++ js/pokemon.js
@@ -4,22 +4,23 @@ var obj;
 
 $button.on("click", function(e) {
   e.preventDefault();
-  $.get('http://pokeapi.co/api/v2/pokemon/' + $textBoxPokemon.val()+"/", function getPokemon (response){  
+  $.get('http://pokeapi.co/api/v1/pokemon/' + $textBoxPokemon.val()+"/", function getPokemon (response){  
+  obj = response;
   var types = response.types;
+  var descriptionUri = response.descriptions[1].resource_uri;
+  var urlApi = "http://pokeapi.co" + descriptionUri;
   $('.row-bg').removeAttr("hidden");
   $('.empty').children().empty();
   $('h2').append("Name: " + response.name);
   $('h3').append("Type: ");
   response.types.forEach(function(element,i,types){  
-     $('h3').append(types[i].type.name +" ");
+     $('h3').append(types[i].name +" ");
    })
   $('h4').append("Description: ");
-  $('ul').append('<li>');
-  $('ul').children().eq(0).addClass('Height');
-  $('.Height').append("Height: " + response.height);
-  $('ul').append('<li>');
-  $('ul').children().eq(1).addClass('Weight');
-  $('.Weight').append("Weight: " + response.weight);
-  obj = response;
+  $.get(urlApi, function getDescription( response){
+    $('.desc').append(response.description);
+
+    });
+  
   });
 });
