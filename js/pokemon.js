d08969bbc58a75ec48ffe05bc66425dc88e85435
mvicariaf <amivicaria@hotmail.es>
2017-07-20 17:46:53 +0200

    iteracion 2 fea

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,11 +1,13 @@
 var $textBoxPokemon = $('#searchPokemon');
-var $button = $('#btn-pokemon');
+var $textBoxType = $('#searchType');
+var $buttonPokemon = $('#btn-pokemon');
+var $buttonType = $('#btn-type');
 var obj;
 
-$button.on("click", function(e) {
+$buttonPokemon.on("click", function(e) {
   e.preventDefault();
   $.get('http://pokeapi.co/api/v1/pokemon/' + $textBoxPokemon.val()+"/", function getPokemon (response){  
-  obj = response;
+ 
   var types = response.types;
   var descriptionUri = response.descriptions[1].resource_uri;
   var urlApi = "http://pokeapi.co" + descriptionUri;
@@ -24,3 +26,24 @@ $button.on("click", function(e) {
   
   });
 });
+
+$buttonType.on("click", function(e) {
+  e.preventDefault();
+ 
+  $.get('http://pokeapi.co/api/v2/type/' + $textBoxType.val()+"/", function getType (response){  
+  var pokemonsNames = [];
+  var pokemons = response.pokemon
+  console.log(response);
+  $('.row-bg').removeAttr("hidden");
+  $('.empty').children().empty();
+  $('h3').append(response.name);
+
+  for (var i=0; i<10; i++ ){
+      pokemonsNames[i] = pokemons[i].pokemon.name;
+      $('ul').append("<li>" + pokemonsNames[i] + "</li>");
+       //inicializa cogiendo la primera imagen del objeto de artista
+    
+  };
+  });
+});
+
